5a9f6f51b1c047b0b6cf304b430b41a00229587e
Nicolás Ponce <nicolas.mponce15@gmail.com>
2025-05-06 00:03:13 -0300

    0.3.1.5 - Mejorado footer

--- src/layouts/footer_layout/Footer_Layout.tsx
+++ src/layouts/footer_layout/Footer_Layout.tsx
@@ -2,6 +2,7 @@ import whatsappIcon from "../../assets/svgs/whatsapp-brands.svg"
 import facebookIcon from "../../assets/svgs/facebook-f-brands.svg"
 import instagramIcon from "../../assets/svgs/instagram-brands.svg"
 import { BlackBlock, BlueBlock, Container, IconsDiv, Line, TextDiv } from "./Footer_Layout.style"
+import {motion} from "framer-motion";
 
 
 
@@ -15,20 +16,26 @@ export const Footer_Layout = () => {
                 <p>Copyright © 2025 Olivos Service. Todos los derechos reservados.</p>
 
                 <IconsDiv>
-                    <a href="#">
+                    <motion.a
+                        whileHover={{y: -4}}
+                    href="#">
                         <img src={whatsappIcon} alt="Icono de Whatsapp" />
-                    </a>
-                    <a href="#">
+                    </motion.a>
+                    <motion.a
+                        whileHover={{y: -4}}                        
+                    href="#">
                         <img src={facebookIcon} alt="Icono de Facebook" />
-                    </a>
-                    <a href="#">
+                    </motion.a>
+                    <motion.a
+                        whileHover={{y: -4}}                    
+                    href="#">
                         <img src={instagramIcon} alt="Icono de Instagram" />
-                    </a>
+                    </motion.a>
                 </IconsDiv>
 
                 <Line />
 
-                <p>Dev & Design by <a href="https://nicolasponce.vercel.app/">Nicolás Ponce</a></p>
+                <p>Dev & Design by <motion.a initial={{opacity: 0.8}} whileHover={{ opacity: 1 }} href="https://nicolasponce.vercel.app/">Nicolás Ponce</motion.a></p>
             </TextDiv>
         </Container>   
     )
